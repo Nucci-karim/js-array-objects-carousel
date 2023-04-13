4cb9e1d0c079301ef817a8edc9fa53e83b37535c
Nucci-karim <nuccikarim@gmail.com>
2023-04-13 16:27:21 +0200

    aggiunta della base con l'esercizio vecchio

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,4 +23,54 @@ const images = [
     }
 ];
 
+// per inserire le immagini
 const linkImg = "./assets/js/"
+
+// --------------------------------------- esercizio vecchio --------------------------------------------//
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+
+console.log( prev, next )
+
+next.addEventListener( 'click', function(){
+    
+    
+    let activeItem = document.querySelector('.item.active')
+    console.log( activeItem )
+
+    let itemToActive = activeItem.nextElementSibling
+    console.log( itemToActive )
+
+    if( activeItem.classList.contains('last') ){
+        itemToActive = document.querySelector('.item.first')
+    }
+
+    activeItem.classList.remove('active')
+
+    
+    itemToActive.classList.add('active')
+
+})
+
+prev.addEventListener( 'click', function(){
+    
+    let activeItem = document.querySelector('.item.active')
+    console.log( activeItem )
+
+    let itemToActive = activeItem.previousElementSibling
+    console.log( itemToActive )
+
+    if( activeItem.classList.contains('first') ){
+        itemToActive = document.querySelector('.item.last')
+    }
+
+    
+    activeItem.classList.remove('active')
+    
+
+    itemToActive.classList.add('active')
+
+})
+
+
+console.log( document.querySelectorAll('.item.last, .prev') )
